b22626cecd3fb424bac0488e9af332f2533a48b1
Thiago Silva <thiagof89.tf@gmail.com>
2022-11-06 10:46:51 -0300

    add mobile folder

---
+++ mobile/App.tsx
@@ -0,0 +1,32 @@
+import { StatusBar } from 'react-native';
+import { NativeBaseProvider } from 'native-base';
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_500Medium,
+  Roboto_700Bold,
+} from '@expo-google-fonts/roboto';
+
+import { THEME } from './src/styles/theme';
+import { Loading } from './src/components/Loading';
+import { SignIn } from './src/screens/SignIn';
+
+export default function App() {
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_500Medium,
+    Roboto_700Bold,
+  });
+
+  return (
+    <NativeBaseProvider theme={THEME}>
+      <StatusBar
+        barStyle='light-content'
+        translucent
+        backgroundColor='transparent'
+      />
+
+      {!fontsLoaded ? <Loading /> : <SignIn />}
+    </NativeBaseProvider>
+  );
+}
---
+++ mobile/src/screens/SignIn.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Center, Text } from 'native-base';
+
+export const SignIn: React.FC = () => {
+  return (
+    <Center flex={1} bgColor='gray.900'>
+      <Text fontSize={24} color='white'>
+        Hello world
+      </Text>
+    </Center>
+  );
+};
